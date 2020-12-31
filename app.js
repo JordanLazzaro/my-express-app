90d49356911700bcf9802aad615bf6ef64743d00
Jordan Lazzaro <jordanlazzaro@jordans-mbp.lan>
2020-12-30 18:29:03 -0600

    Added listen

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
+const port = 8080
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world')
 })
+
+app.listen(port, () => {
+  console.log(`App running on port: 8080`)
+})
+
+
