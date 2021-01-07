4b596914802dd1958bed2784417d73b4cfdb00f5
Jordan Lazzaro <jordantlazzaro@gmail.com>
2021-01-06 20:37:48 -0500

    Added api endpoints for files and filename

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const express = require('express')
+const { Storage } = require('@google-cloud/storage')
+const { port, bucketName } = require('./config.json')
 const app = express()
-const port = 8080
+const storage = new Storage({keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS})
+const bucket = storage.bucket(bucketName)
 
-// respond with "hello world" when a GET request is made to the homepage
+
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.send('Welcome Home!')
 })
 
+app.route('/api/files')
+  .get(async function (req, res) {
+    const [file_objs] = await bucket.getFiles()
+    var file_names = []
+    file_objs.forEach(file => {
+      file_names.push(file.name)
+    })
+    res.send({file_names})
+  })
+
+app.route('/api/files/:filename')
+  .get(async function (req, res) {
+    var file = bucket.file(req.params.filename)
+    file.createReadStream()
+    .on('error', (err) => {
+      res.status(404).end()
+    })
+    .pipe(res)
+  })
+  .post(async function (req, res) {
+    var filename = req.params.filename
+    // TODO: send file to storage
+  })
+
 app.listen(port, () => {
-  console.log(`App running on port: 8080`)
+  console.log(`App running on port: ${port}`)
 })
 
 
